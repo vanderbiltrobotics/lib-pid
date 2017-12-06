7f3aedc13e7e9ef5c473fa1e4eeb4d70b2c90c37
Dwatkins <dexter.a.watkins@vanderbilt.edu>
2017-12-06 12:00:33 -0600

    updated pid for clamped output

--- src/pid/include/pid/pid.h
+++ src/pid/include/pid/pid.h
@@ -1,91 +1,119 @@
-class libpid
+#ifndef PID_H
+#define PID_H
+
+class PID
 {
  public:
-  libpid()
-  {
-    kp_ = ki_ = kd_ = derivator_ = integrator_ = integrator_max_ = integrator_min_ = set_point_ = 0;
-  }
-
-  libpid(double p,
-	 double i,
-	 double d,
-	 double derivator,
-	 double integrator,
-	 double integrator_max,
-   double integrator_min)
-   {
-    kp_ = p;
-    ki_ = i;
-    kd_ = d;
-    derivator_ = derivator;
-    integrator_ = integrator;
-    integrator_max_ = integrator_max;
-    integrator_min_ = integrator_min;
-  }
-
-  double update(double current_value){
-    double error = set_point_ - current_value;
-    double p_value = kp_ * error;
-    double d_value = kd_ * (error - derivator_);
-    derivator_ = error;
-    integrator_ = integrator_ + error;
-    if (integrator_ > integrator_max_)
-      integrator_ = integrator_max_;
-    else if (integrator_ < integrator_min_)
-      integrator_ = integrator_min_;
-    double i_value = integrator_ * ki_;
-    double pid = p_value + i_value + d_value;
-    return pid;
-  }
-
-  void setPoint(double set_point){
-    set_point_ = set_point;
-  }
-
-  void setIntegrator(double integrator){
-    integrator_ = integrator;
-  }
-
-  void setIntegratorMax(double max){
-    integrator_max_ = max;
-  }
+  PID();
+  ~PID() {}
+  PID(double p, 
+      double i, 
+      double d, 
+      double derivator, 
+      double integrator,
+      double integrator_max,
+      double integrator_min);
   
-  void setIntegratorMin(double min) {
+  PID& operator=(const PID& rhs);
+  
+  double update(double current_value)
+  {
+    double error = set_point_ - current_value;
+    integrator_ += error;
+    //clamp integrator_ value between integrator_max_ and integrator_min_
+    double i_value = ki_ * (integrator_ > integrator_max_ ? integrator_max_ : (integrator_ < integrator_min_ ? integrator_min_ : integrator_));
+    double pid = kp_*error + kd_*(error-derivator_) + i_value;
+    if(isLimited_)
+      {
+        //clamp pid value between output_max_ and output_min_
+        pid = pid > output_max_ ? output_max_ : (pid < output_min_ ? output_min_ : pid);
+      }
+    derivator_ = error;
+    return pid;  
+  };
+  
+  void setPoint(double set_point)
+  {
+    set_point_ = set_point;
+  };
+  
+  void setIntegrator(double integrator)
+  {
+    integrator_ = integrator;
+  };
+  
+  void setIntegratorMax(double max)
+  {
+    integrator_max_ = max;
+  };
+  
+  void setIntegratorMin(double min)
+  {
     integrator_min_ = min;
-  }
+  };
+  
+  void setDerivator(double derivator)
+  {
+    derivator_ = derivator;
+  };
+  
+  void setKp(double kp)
+  {
+    kp_ = kp;
+  };
+  
+  void setKi(double ki)
+  {
+    ki_ = ki;
+  };
+  
+  void setKd(double kd)
+  {
+    kd_ = kd;
+  };
+  
+  void limitOutput(double min, double max)
+  {
+    isLimited_ = true;
+    output_min_ = min;
+    output_max_ = max;
+  };
 
-  void setKp(double kp){
-    kp_ = (kp >= 0) ? kp : 0;
-  }
-
-  void setKi(double ki){
-    ki_ = (ki >= 0) ? ki : 0;
-  }
-
-  void setKd(double kd){
-    kd_ = (kd >= 0) ? kd : 0;
-  }
-  double getKp(){
-    return kp_;
-  }
-  double getKd(){
-    return kd_;
-  }
-  double getKi(){
-    return ki_;
-  }
-
-  double getPoint(){
+  void delimitOutput()
+  {
+    isLimited_ = false;
+  };
+  
+  double getPoint()
+  {
     return set_point_;
-  }
-
-  double getIntegrator(){
+  };
+  
+  double getIntegrator()
+  {
     return integrator_;
-  }
-  double getDerivator(){
+  };
+  
+  double getDerivator()
+  {
     return derivator_;
-  }
+  };
+  
+  double getOutputMin()
+  {
+    return output_min_;
+  };
+  
+  double getOutputMax()
+  {
+    return output_max_;
+  };
 
+  bool outputIsLimited()
+  {
+    return isLimited_;
+  };
+  
  private:
   double kp_;
   double ki_;
@@ -95,4 +123,9 @@ class libpid
   double integrator_min_;
   double integrator_max_;
   double set_point_;
+  double output_max_;
+  double output_min_;
+  bool isLimited_;
 };
+
+#endif
