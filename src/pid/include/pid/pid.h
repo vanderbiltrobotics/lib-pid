ba57718c9ee26dbf9bd5906a637d544d145a9f83
Dexter Watkins <dexter.a.watkins@vanderbilt.edu>
2018-05-24 15:46:32 -0500

    fixed constructors to work with new deadzone

--- src/pid/include/pid/pid.h
+++ src/pid/include/pid/pid.h
@@ -3,8 +3,12 @@
 
 class PID
 {
- public:
-  PID();
+public:
+  PID()
+  {
+    kp_ = ki_ = kd_ = derivator_ = integrator_ = integrator_max_ = integrator_min_ = set_point_ = output_max_ = output_min_ = deadzone_lower_ = deadzone_upper_ = 0;
+    isLimited_ = hasDeadzone_ = false;
+  };
   ~PID() {}
   PID(double p, 
       double i, 
@@ -12,9 +16,42 @@ class PID
       double derivator, 
       double integrator,
       double integrator_max,
-      double integrator_min);
+      double integrator_min)
+  {
+    kp_ = p;
+    ki_ = i;
+    kd_ = d;
+    derivator_ = derivator;
+    integrator_ = integrator;
+    integrator_max_ = integrator_max;
+    integrator_min_ = integrator_min;
+    output_max_ = 0;
+    output_min_ = 0;
+    deadzone_lower_ = 0;
+    deadzone_upper_ = 0;
+    isLimited_ = false;
+    hasDeadzone = false;
+  };
   
-  PID& operator=(const PID& rhs);
+  PID& operator=(const PID& rhs)
+  {
+    if (this == &rhs)
+      return *this;
+    kp_ = rhs.kp_;
+    ki_ = rhs.ki_;
+    kd_ = rhs.kd_;
+    set_point_ = rhs.set_point_;
+    derivator_ = rhs.derivator_;
+    integrator_ = rhs.integrator_;
+    integrator_max_ = rhs.integrator_max_;
+    integrator_min_ = rhs.integrator_min_;
+    output_min_ = rhs.output_min_;
+    output_max_ = rhs.output_max_;
+    isLimited_ = rhs.isLimited_;
+
+    return *this;
+  };
+
   
   double update(double current_value)
   {
@@ -166,7 +203,7 @@ class PID
     return hasDeadzone_;
   };
   
- private:
+private:
   double kp_;
   double ki_;
   double kd_;
